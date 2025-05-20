507ca8a4ca2d95a291db5d950259c19ba3635933
dz-mykolas <dz.mykolas@gmail.com>
2025-05-20 21:36:29 +0300

    added Dockerfile, proxy, polling hot reload, fixed api route

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,19 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    vite: {
+        server: {
+            watch: {
+                usePolling: true,
+                interval: 1000,
+            },
+            proxy: {
+                '/api': {
+                    target: 'http://academia-backend:8080',
+                    changeOrigin: true,
+                },
+            },
+        },
+    },
+});
